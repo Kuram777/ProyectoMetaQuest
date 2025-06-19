1593090b30e9cf4748a3952b173309688cd6f66d
Kuram7 <luis.sierra.mmld00@gmail.com>
2025-06-19 18:56:46 -0500

    Programación de movimiento del personaje en eje X y Z

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,23 @@
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-  CharacterController characterController;
-  public int speed = 7;
-  
+    CharacterController characterController;
+    public int speed = 7;
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+    private void Update()
+    {
+        PlayerMovement();
+    }
+    void PlayerMovement()
+    {
+        float x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float z = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        Vector3 movement = Vector3.right * x + Vector3.forward * z;
+        characterController.Move(movement);
+    }
 }
