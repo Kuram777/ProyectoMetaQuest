13b368e77019ad5de1569acab7bac87aae1e3c30
Kuram7 <luis.sierra.mmld00@gmail.com>
2025-06-19 19:17:38 -0500

    Movimiento de la camara realizado

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,9 @@ public class PlayerController : MonoBehaviour
 {
     CharacterController characterController;
     public int speed = 7;
+    public Transform CameraPostion;
+    public int sensitivity = 77;
+    float xRotation = 0f;
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
@@ -12,6 +15,7 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         PlayerMovement();
+        CameraMovement();
     }
     void PlayerMovement()
     {
@@ -19,5 +23,14 @@ public class PlayerController : MonoBehaviour
         float z = Input.GetAxis("Vertical") * speed * Time.deltaTime;
         Vector3 movement = Vector3.right * x + Vector3.forward * z;
         characterController.Move(movement);
+    }
+    void CameraMovement()
+    {
+        float xMouse = Input.GetAxis("Mouse X") * sensitivity *Time.deltaTime;
+        float yMouse = Input.GetAxis("Mouse Y") * sensitivity*Time.deltaTime;
+        xRotation = xRotation - yMouse;
+        xRotation = Mathf.Clamp(xRotation, -77f, 90f);
+        CameraPostion.localRotation = Quaternion.Euler(xRotation,0,0);
+        transform.Rotate(Vector3.up * xMouse);
     }
 }
